1d84a29a5c5e309d278c22a1012578c75746078d
Fin Brown <fglb2@cam.ac.uk>
2018-02-09 15:43:06 +0000

    Added choice of run

--- DynamicTable/Program.cs
+++ DynamicTable/Program.cs
@@ -16,7 +16,8 @@ namespace DynamicTable
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Application.Run(new UI_Base());
         }
     }
 }
